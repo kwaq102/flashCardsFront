38f3539164a7ca56361ba70c1814a9899ed8fb6d
Kamil Gajewski <kwaq102@gmail.com>
2023-04-19 10:53:33 +0200

    added sort all words

--- src/components/WordsComponents/DisplayWords.tsx
+++ src/components/WordsComponents/DisplayWords.tsx
@@ -6,6 +6,28 @@ interface Props {
 }
 
 const DisplayWords = ({ words }: Props) => {
+	const allWords = words
+		.sort((prev, curr) => {
+			if (prev.title.toUpperCase() < curr.title.toUpperCase()) {
+				return -1;
+			} else if (prev.title.toUpperCase() > curr.title.toUpperCase()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		})
+		.map((word, i) => (
+			<tr key={word.id} className="displayAllWords__table__body__row">
+				<th className="displayAllWords__table__body__element ordinal-number">
+					{i + 1}
+				</th>
+				<th className="displayAllWords__table__body__element"> {word.title}</th>
+				<th className="displayAllWords__table__body__element">
+					{word.description}
+				</th>
+				<th className="displayAllWords__table__body__element">{word.notes}</th>
+			</tr>
+		));
 	return (
 		<section className="displayAllWords">
 			<h2 className="displayAllWords__heading headingH3">Twój słownik</h2>
@@ -25,25 +47,7 @@ const DisplayWords = ({ words }: Props) => {
 						</th>
 					</tr>
 				</thead>
-				<tbody className="displayAllWords__table__body">
-					{words.map((word, i) => (
-						<tr key={word.id} className="displayAllWords__table__body__row">
-							<th className="displayAllWords__table__body__element ordinal-number">
-								{i + 1}
-							</th>
-							<th className="displayAllWords__table__body__element">
-								{" "}
-								{word.title}
-							</th>
-							<th className="displayAllWords__table__body__element">
-								{word.description}
-							</th>
-							<th className="displayAllWords__table__body__element">
-								{word.notes}
-							</th>
-						</tr>
-					))}
-				</tbody>
+				<tbody className="displayAllWords__table__body">{allWords}</tbody>
 			</table>
 		</section>
 	);
