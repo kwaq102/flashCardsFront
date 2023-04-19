276432c10661b01602d6954b94c5005035425304
Kamil Gajewski <kwaq102@gmail.com>
2023-04-19 10:37:08 +0200

    setting display words in table and added styles

--- src/components/WordsComponents/DisplayWords.tsx
+++ src/components/WordsComponents/DisplayWords.tsx
@@ -5,21 +5,46 @@ interface Props {
 	words: WordEntity[];
 }
 
-// TODO chyba zrobić tabelkę ze słówkami
-
 const DisplayWords = ({ words }: Props) => {
 	return (
-		<section>
-			<h2>Twój słownik</h2>
-			{words.map((word, i) => (
-				<div key={word.id}>
-					<h3>
-						{i + 1} {word.title}
-					</h3>
-					<p>{word.description}</p>
-					<p>{word.notes}</p>
-				</div>
-			))}
+		<section className="displayAllWords">
+			<h2 className="displayAllWords__heading headingH3">Twój słownik</h2>
+
+			<table className="displayAllWords__table">
+				<thead className="displayAllWords__table__heading">
+					<tr className="displayAllWords__table__heading__row">
+						<th className="displayAllWords__table__heading__element ordinal-number-head">
+							L.p.
+						</th>
+						<th className="displayAllWords__table__heading__element">Tytuł</th>
+						<th className="displayAllWords__table__heading__element">
+							Znaczenie
+						</th>
+						<th className="displayAllWords__table__heading__element">
+							Notatki
+						</th>
+					</tr>
+				</thead>
+				<tbody className="displayAllWords__table__body">
+					{words.map((word, i) => (
+						<tr key={word.id} className="displayAllWords__table__body__row">
+							<th className="displayAllWords__table__body__element ordinal-number">
+								{i + 1}
+							</th>
+							<th className="displayAllWords__table__body__element">
+								{" "}
+								{word.title}
+							</th>
+							<th className="displayAllWords__table__body__element">
+								{word.description}
+							</th>
+							<th className="displayAllWords__table__body__element">
+								{word.notes}
+							</th>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</section>
 	);
 };
