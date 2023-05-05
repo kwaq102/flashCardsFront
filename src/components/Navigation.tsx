c8b3d19a6b06b3a6872cff1c479657a12317f200
Kamil Gajewski <kwaq102@gmail.com>
2023-05-05 13:26:57 +0200

    added rwd

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { navList } from "../utils/navigationList";
 import { UserEntity } from "types";
+
+import arrow from "../img/arrow-nav.svg";
 
 interface Props {
 	user: UserEntity | null;
 }
 
 const Navigation = ({ user }: Props) => {
+	const [showNav, setShowNav] = useState(false);
 	const userIdPath = user?.id ? user.id : "../login";
 
 	const navigation = navList.map(el => (
-		<li className="navigation__list__element-link">
+		<li key={el.navName} className="navigation__list__element-link">
 			<NavLink
 				to={el.path}
 				end={el.end}
 				className="navigation__list__element-link"
+				onClick={() => setShowNav(false)}
 			>
 				{el.navName}
 			</NavLink>
@@ -24,13 +28,14 @@ const Navigation = ({ user }: Props) => {
 
 	return (
 		<nav className="navigation">
-			<ul className="navigation__list">
+			<ul className={showNav ? "navigation__list show" : "navigation__list"}>
 				{navigation}
 				{/* TODO spróbować to ogarnąć aby korzystać normalnie z user/:id */}
 				<li className="navigation__list__element-link">
 					<NavLink
 						to={`/user/${userIdPath}`}
 						className="navigation__list__element-link"
+						onClick={() => setShowNav(false)}
 					>
 						Strona użytkownika
 					</NavLink>
@@ -41,6 +46,7 @@ const Navigation = ({ user }: Props) => {
 							<NavLink
 								to={`/user/${userIdPath}/add-word`}
 								className="navigation__list__element-link"
+								onClick={() => setShowNav(false)}
 							>
 								Dodaj słowo
 							</NavLink>
@@ -49,6 +55,7 @@ const Navigation = ({ user }: Props) => {
 							<NavLink
 								to={`/user/${userIdPath}/show-words`}
 								className="navigation__list__element-link"
+								onClick={() => setShowNav(false)}
 							>
 								Pokaż słownik
 							</NavLink>
@@ -56,6 +63,17 @@ const Navigation = ({ user }: Props) => {
 					</>
 				)}
 			</ul>
+
+			<div
+				className={showNav ? "navigation__arrow" : "navigation__arrow show"}
+				onClick={() => setShowNav(!showNav)}
+			>
+				<img
+					src={arrow}
+					alt="strzałka pokaż/ukryj menu"
+					className="navigation__arrow-img"
+				/>
+			</div>
 		</nav>
 	);
 };
--- src/components/WordsComponents/DisplayWords.tsx
+++ src/components/WordsComponents/DisplayWords.tsx
@@ -1,8 +1,7 @@
-import React, { FormEvent, MouseEvent, useEffect, useState } from "react";
-import { MAIN_URL } from "../../utils/url";
+import React, { useEffect, useState } from "react";
 import { WordEntity } from "types";
 import TopArrow from "../TopArrow";
-import Pagination from "../Pagination/Pagination";
+import Table from "./Table";
 
 interface Props {
 	words: WordEntity[];
@@ -10,127 +9,7 @@ interface Props {
 }
 
 const DisplayWords = ({ words, onWordsChange }: Props) => {
-	const [edit, setEdit] = useState(false);
-	const [form, setForm] = useState({
-		id: "",
-		title: "",
-		description: "",
-		notes: "",
-	});
-	const [marginForm, setMarginForm] = useState(0);
 	const [isArrow, setIsArrow] = useState(false);
-
-	const [currentPage, setCurrentPage] = useState(1);
-	const [wordsPerPage, setWordsPerPage] = useState(15);
-	const removeWord = async (e: MouseEvent<HTMLButtonElement>) => {
-		if (!window.confirm("Czy na pewno?")) return;
-
-		const [wordToRemove] = words.filter(
-			word => word.id === e.currentTarget.getAttribute("data-id")
-		);
-		const res = await fetch(`${MAIN_URL}/data/remove/${wordToRemove.id}`, {
-			method: "DELETE",
-		});
-
-		if (res.status === 200 || res.status === 204) {
-			onWordsChange();
-		}
-	};
-
-	const editWordOn = (e: MouseEvent) => {
-		const heightFromTopForm = Number(
-			e.currentTarget.parentElement?.parentElement?.offsetTop
-		);
-
-		setMarginForm(heightFromTopForm);
-		setEdit(true);
-		const [wordToEdit] = words.filter(
-			word => word.id === e.currentTarget.getAttribute("data-id")
-		);
-
-		if (wordToEdit.id === undefined) return null;
-
-		setForm({
-			id: wordToEdit?.id,
-			title: wordToEdit?.title,
-			description: wordToEdit?.description,
-			notes: wordToEdit?.notes,
-		});
-	};
-
-	const editWord = async (e: FormEvent) => {
-		e.preventDefault();
-
-		// TODO ten confirm zamienić na jakiś ładny popup
-		try {
-			if (!window.confirm("Czy chcesz zapisać zmiany?")) return;
-			await fetch(`${MAIN_URL}/data/search/${form.id}`, {
-				method: "PATCH",
-				headers: {
-					"Content-type": "application/json",
-				},
-				body: JSON.stringify({
-					id: form.id,
-					title: form.title,
-					description: form.description,
-					notes: form.notes,
-				}),
-			});
-		} catch (e) {
-			console.error(e);
-		}
-
-		setEdit(false);
-		onWordsChange();
-		// TODO Poszkać i przypomnieć skąd sie bierze ten refresh
-	};
-	const indexOfLastWord = currentPage * wordsPerPage;
-	const indexOfFirstWord = indexOfLastWord - wordsPerPage;
-	const allWords = words
-		.sort((prev, curr) => {
-			if (prev.title.toUpperCase() < curr.title.toUpperCase()) {
-				return -1;
-			} else if (prev.title.toUpperCase() > curr.title.toUpperCase()) {
-				return 1;
-			} else {
-				return 0;
-			}
-		})
-		.map((word, i) => (
-			<tr
-				key={word.id}
-				className="displayAllWords__table__body__row"
-				data-index={i + 1}
-			>
-				<th className="displayAllWords__table__body__element ordinal-number">
-					{i + 1}
-				</th>
-				<th className="displayAllWords__table__body__element">{word.title}</th>
-				<th className="displayAllWords__table__body__element">
-					{word.description}
-				</th>
-				<th className="displayAllWords__table__body__element">{word.notes}</th>
-				<th className="displayAllWords__table__body__element actions">
-					<button
-						className="displayAllWords__table__body__element-edit btn"
-						onClick={editWordOn}
-						data-id={word.id}
-						data-index={i + 1}
-					>
-						Edytuj
-					</button>
-					<button
-						className="displayAllWords__table__body__element-delete btn"
-						onClick={removeWord}
-						data-id={word.id}
-					>
-						X
-					</button>
-				</th>
-			</tr>
-		));
-
-	const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
 	useEffect(() => {
 		window.addEventListener("scroll", showArrow);
@@ -139,13 +18,6 @@ const DisplayWords = ({ words, onWordsChange }: Props) => {
 			window.removeEventListener("scroll", showArrow);
 		};
 	}, []);
-
-	const updateForm = (key: string, value: string) => {
-		setForm(form => ({
-			...form,
-			[key]: value,
-		}));
-	};
 
 	const showArrow = () => {
 		if (window.pageYOffset >= 250) {
@@ -158,103 +30,7 @@ const DisplayWords = ({ words, onWordsChange }: Props) => {
 	return (
 		<section className="displayAllWords">
 			<h2 className="displayAllWords__heading headingH3">Twój słownik</h2>
-			<div className="displayAllWords__table-wrapper">
-				<table className="displayAllWords__table">
-					<thead className="displayAllWords__table__heading">
-						<tr className="displayAllWords__table__heading__row">
-							<th className="displayAllWords__table__heading__element ordinal-number-head">
-								L.p.
-							</th>
-							<th className="displayAllWords__table__heading__element title">
-								Tytuł
-							</th>
-							<th className="displayAllWords__table__heading__element">
-								Znaczenie
-							</th>
-							<th className="displayAllWords__table__heading__element">
-								Notatki
-							</th>
-						</tr>
-					</thead>
-					<tbody className="displayAllWords__table__body">
-						{allWords.slice(indexOfFirstWord, indexOfLastWord)}
-					</tbody>
-				</table>
-
-				<Pagination
-					wordsPerPage={wordsPerPage}
-					totalWords={allWords.length}
-					paginate={paginate}
-					currentPage={currentPage}
-				/>
-
-				{edit && (
-					<div className="displayAllWords__edit__form-wrapper">
-						<form
-							onSubmit={editWord}
-							className="displayAllWords__edit__form"
-							style={{
-								top: marginForm,
-							}}
-						>
-							<label className="displayAllWords__edit__form__label">
-								<fieldset className="displayAllWords__edit__form__fieldset">
-									<legend className="displayAllWords__edit__form__legend">
-										Title
-									</legend>
-
-									<input
-										type="text"
-										value={form.title}
-										name="title"
-										onChange={e => updateForm("title", e.target.value)}
-										className="displayAllWords__edit__form__input"
-									/>
-								</fieldset>
-							</label>
-							<label className="displayAllWords__edit__form__label">
-								<fieldset className="displayAllWords__edit__form__fieldset">
-									<legend className="displayAllWords__edit__form__legend">
-										Description
-									</legend>
-									<input
-										type="text"
-										name="description"
-										value={form.description}
-										onChange={e => updateForm("description", e.target.value)}
-										className="displayAllWords__edit__form__input"
-									/>
-								</fieldset>
-							</label>
-							<label className="displayAllWords__edit__form__label">
-								<fieldset className="displayAllWords__edit__form__fieldset">
-									<legend className="displayAllWords__edit__form__legend">
-										Notes
-									</legend>
-									<textarea
-										name="notes"
-										value={form.notes}
-										onChange={e => updateForm("notes", e.target.value)}
-										className="displayAllWords__edit__form__textarea"
-									/>
-								</fieldset>
-							</label>
-							<button
-								type="submit"
-								className="displayAllWords__edit__form__btn btn"
-							>
-								Zapisz
-							</button>
-							<button
-								onClick={() => setEdit(false)}
-								className="displayAllWords__edit__form__btn-close btn"
-							>
-								Anuluj
-							</button>
-						</form>
-					</div>
-				)}
-			</div>
+			<Table words={words} onWordsChange={onWordsChange} />
 			{isArrow && <TopArrow />}
 		</section>
 	);
